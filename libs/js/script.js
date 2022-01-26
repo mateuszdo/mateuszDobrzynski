83aee8a26c24eeb359740101bca40f4ae17ca2b6
mateusz <mateusz@192.168.1.101>
2022-01-26 14:06:09 +0000

    sidebar working

--- libs/js/script.js
+++ libs/js/script.js
@@ -56,6 +56,16 @@ function getLocation(lat, lng) {
     })
 };
 
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+};
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+};
+
 function getSelect() {
     $.getJSON("libs/php/getSelect.php", function(result){
         $("#getAllCountries").html("hey");
