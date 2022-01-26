c7c2e5ba685f46eb76a237c2fa694e4c3a1c964c
mateusz <mateusz@192.168.1.101>
2022-01-26 11:17:59 +0000

    get select array struggle

--- libs/php/getSelect.php
+++ libs/php/getSelect.php
@@ -2,14 +2,18 @@
       $url = file_get_contents("libs/json/countryBorders.geo.json");
       $arr = json_decode($url,true);
     
-  
+    $countries = array();
     foreach($arr['features'] as $value)  {
              $name = $value['properties']['name'];
              $iso =  $value['properties']['iso_a2'];
-
+             /*
              $option =  "<option value=$iso>$name</option>";
              $json_option = json_encode($option);
              echo $json_option;
+             */
+            
+            array_push($countries, $name, $iso);
+            print_r($countries);
        };
        
 ?>
